173750cfea2f4c78b5041ed115476b5765f375f1
DaniilAfan <110550038+DaniilAfan@users.noreply.github.com>
2022-09-22 22:24:55 +0300

    Код для решения контрольной работы

---
+++ ex1/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] array1 = new string[4] {"Yes", "No", "123", "1234"};
+string[] array2 = new string[array1.Length];
+void SimvolCheckout(string[] array1, string[] array2)
+{
+    int change = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[change] = array1[i];
+        change++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SimvolCheckout(array1, array2);
+PrintArray(array2);
+
